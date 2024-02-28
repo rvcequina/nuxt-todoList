e3bc28759e8a0e309d8309e1bffa7999bde17fd3
Ryan Vincent Cequina <ryan.cequina@awsys-i.com>
2024-02-28 15:19:05 +0800

    configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    vue: {
+      customElement: true
+    },
+    vueJsx: {
+      mergeProps: true
+    }
+  }
 })
